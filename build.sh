5cc4e9e3e1585a5611d23aa6d2828ec16dc7a609
Noah Theus <noaht@watn3y.de>
2023-09-10 14:06:48 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
- ###!/usr/bin/env bash
-
-read -p 'Tag?: ' tag
-
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag watn3y/fritzbox-tools:$(git log -1 --pretty=%h) --tag watn3y/fritzbox-tools:$tag --build-arg=COMMIT=$(git log -1 --pretty=%h) .
-
